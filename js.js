916f89fbb531a60cae85e6c797df487625308264
Mason Koppel <mkoppel12@outlook.com>
2024-12-07 18:42:06 -0500

    Added Server

--- js.js
+++ js.js
@@ -1,6 +1,28 @@
+const express=require("express");
+const nodemailer=require("nodemailer");
+const cors=require("cors");
+const bodyparser=require("body-parser");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'orderStyle.css')));
+app.use(express.static(path.join(__dirname, 'FrenchFriesStyle.css')));
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, 'Home.html'));
+});
+app.get("/order", (req, res) => {
+    res.sendFile(path.join(__dirname, 'Order.html'));
+});
+app.get("/aboutus", (req, res) => {
+    res.sendFile(path.join(__dirname, 'AboutUs.html'));
+});
+
 let orders = [];
 let total = 0;
-
 function addToOrder(name, price) {
     orders.push({name: name, price: price});
 
@@ -35,3 +57,7 @@ function removeItem(index) {
 
     updateOrderSummary();
 }
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
